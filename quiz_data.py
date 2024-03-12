e9c6088d11a9975ed6126f31038da6137fe5f509
Bhargavi <163198009+nbhargavi-24@users.noreply.github.com>
2024-03-13 02:08:38 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,53 @@
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Paris", "London", "Berlin", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Which is the largest ocean?",
+        "choices": ["Indian", "Artic", "Pacific", "American"],
+        "answer": "Pacific"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "choices": ["Go", "Au", "Ag", "Gd"],
+        "answer": "Au"
+    },
+    {
+        "question": "Which country is known as the 'Land of the Rising Sun'?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "When was Netflix founded ?",
+        "choices" : ["1997","2001", "2009", "2015"],
+        "answer" : "1997"
+    },
+    {
+        "question": "Who is 3rd on the all-time list of female tennis Grand Slam champions?",
+        "choices" : ["Sania Mirza","Serena Williams", "Selena Gomes", "Saina Nehwal"],
+        "answer" : "Serena Williams"
+    },
+    {
+        "question": "What is the only continent with land in all four hemispheres?",
+        "choices" : ["New Zealand","Jamica", "Australia", "Africa"],
+        "answer" : "Africa"
+    },
+    {
+        "question": "Who was the first Disney princess?",
+        "choices" : ["Ariel","Rapunzel", "Snow White", "Moana"],
+        "answer" : "Snow White"
+    },
+    {
+        "question": "What is a group of crows called?",
+        "choices" : ["A Murder","A Flock", "A Pride", "A Parade"],
+        "answer" : "A Murder"
+    },
+    {
+        "question": "Where is the strongest human muscle located?",
+        "choices" : ["Spine","Jaw", "Elbow", "Intestine"],
+        "answer" : "Jaw"
+    }
+    # Add more questions here
+]
